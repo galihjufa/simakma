cd5528b327010ce4e3328489509196674f780b27
Galih Jufa Wirahadi Laksana <galihjufa@gmail.com>
2018-01-02 10:53:44 +0700

    penduduk

--- app/Penduduk.php
+++ app/Penduduk.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Penduduk extends Model
 {
     protected $fillable = [
-        'nik', 'nama','provinsi','kabupaten','kecamatan','desa', 'pekerjaan', 'pendapatan', 'jumlah_anggota_keluarga','bantuan'
+        'nik', 'nama','provinsi','kabupaten','kecamatan','desa', 'pekerjaan', 'pendapatan', 'jumlah_anggota_keluarga'
     ];
 }
